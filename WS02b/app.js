6b9c944901991ae610580b302f267fc813c7c5c8
Janette033 <janetterautiainen@hotmail.com>
2025-02-19 10:48:05 +0200

    Lisätty koodia

---
+++ WS02b/app.js
@@ -0,0 +1,12 @@
+const axios = require('axios');
+
+async function fetchData() {
+    try {
+        const response = await axios.get('https://jsonplaceholder.typicode.com/todos/1');
+        console.log('Data:', response.data);
+    } catch (error) {
+        console.error('Virhe haettaessa dataa:', error);
+    }
+}
+
+fetchData();
