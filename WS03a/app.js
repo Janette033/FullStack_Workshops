ee194a764fb2f95baad819935c0928f72da47669
Janette033 <janetterautiainen@hotmail.com>
2025-03-06 13:38:58 +0200

    Luotu app.js, jossa alustettu express ja kuunnellaan porttia 3000

---
+++ WS03a/app.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const app = express()
+const port = 3000;
+
+app.get('/', (req, res) => {
+    res.send('Hello, World!');
+});
+
+app.get('/about', (req, res) => {
+    res.send('About Page');
+});
+
+app.get('/contact', (req, res) => {
+    res.send('Contact Page');
+});
+
+app.get('/services', (req, res) => {
+    res.send('Services Page');
+});
+
+app.listen(port, () => {
+    console.log(`Palvelin käynnissä osoitteessa http://localhost:${port}`);
+});
