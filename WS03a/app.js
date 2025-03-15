6aaa535350d18845a9e827cb0b50544f4233bcf8
Janette033 <janetterautiainen@hotmail.com>
2025-03-15 11:04:00 +0200

    Tehty harjoitus 5 koodiin

--- WS03a/app.js
+++ WS03a/app.js
@@ -31,13 +31,13 @@ const express = require("express");
 const app = express();
 const port = 3000;
 const path = require("path");
-
+/*
 app.use(express.static(path.join(__dirname, "public")));
 
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
 });
-
+*/
 app.listen(port, () => {
   console.log(`Palvelin käynnissä osoitteessa http://localhost:${port}`);
 });
@@ -77,4 +77,85 @@ app.use(loggerMiddleware);
 app.post('/testi', customHeaderMiddleware, (req, res) => {
   const requestBody = req.body;
   res.json(requestBody);
+});
+
+// Harjoitus 5
+
+const fs = require('fs');
+
+app.get('/list', (req, res) => {
+    const filePath = path.join(__dirname, 'data.txt');
+    fs.readFile(filePath, 'utf8', (err, data) => {
+      if (err) {
+        console.error("Virhe tiedoston lukemisessa:", err);
+        res.status(500).send('Error reading file');
+      } else {
+        res.send(data);
+      }
+    });
+  });
+
+app.get("/json", (req, res) => {
+    const filePath = path.join(__dirname, "data.json");
+    fs.readFile(filePath, "utf8", (err, data) => {
+      if (err) {
+        console.error("Error JSON tiedoston lukemisessa:", err);
+        return res.status(500).send("Error JSON tiedoston lukemisessa");
+      }
+  
+      try {
+        const jsonData = JSON.parse(data);
+  
+        let html = `<table border="1" cellspacing="0" cellpadding="10">
+                      <tr>
+                        <th>Nimi</th>
+                        <th>Ikä</th>
+                        <th>Kaupunki</th>
+                      </tr>`;
+  
+        jsonData.forEach((item) => {
+          html += `<tr>
+                     <td>${item.name}</td>
+                     <td>${item.age}</td>
+                     <td>${item.city}</td>
+                   </tr>`;
+        });
+  
+        html += `</table>`;
+  
+        res.send(html);
+      } catch (parseError) {
+        console.error("Error JSON datan parsimisessa", parseError);
+        res.status(500).send("Error JSON datan parsimisessa");
+      }
+    });
+  });
+
+app.post('/add', (req, res) => {
+  const newUser = req.body;
+  const filePath = path.join(__dirname, "data.json");
+
+  fs.readFile(filePath, 'utf8', (err, data) => {
+      if (err) {
+          console.error("Error JSON tiedoston lukemisessa:", err);
+          return res.status(500).send("Error JSON tiedoston lukemisessa");
+      }
+
+      try {
+          let users = JSON.parse(data);
+          users.push(newUser);
+
+          fs.writeFile(filePath, JSON.stringify(users, null, 2), (err) => {
+              if (err) {
+                  console.error("Error JSON tiedoston kirjoittamisessa:", err);
+                  return res.status(500).send("Error JSON tiedoston kirjoittamisessa");
+              }
+              res.status(201).json(users);
+          });
+
+      } catch (error) {
+          console.error("Virhe JSON-parsinnassa:", error);
+          return res.status(500).send("Virhe JSON-datan käsittelyssä");
+      }
+  });
 });
