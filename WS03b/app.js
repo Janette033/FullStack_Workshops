4cd2bc400e32b1621b0702892edcf8eba32f7522
Janette033 <janetterautiainen@hotmail.com>
2025-04-06 12:26:50 +0300

    Lisätty tehtävä 5 koodiin

--- WS03b/app.js
+++ WS03b/app.js
@@ -1,4 +1,3 @@
-// Tehtävä 1
 const express = require('express');
 const app = express();
 const PORT = 3000;
@@ -8,8 +7,12 @@ app.set('view engine', 'ejs')
 app.get('/', (req, res) => {
     const items = ['Kohta 1', 'Kohta 2', 'Kohta 3', 'Kohta 4'];
     const isLoggedIn = false;
-    res.render('index', {message: 'Hello world!', items, isLoggedIn});
-
+    const users = [
+        { name: 'Maija Mehiläinen', email: 'maija@example.com' },
+        { name: 'Matti Meikäläinen', email: 'matti@example.com' },
+        { name: 'Liisa Virtanen', email: 'liisa@example.com' }
+      ];
+    res.render('index', {message: 'Hello world!', items, isLoggedIn, users});
 });
 
 app.listen(PORT, () => {
